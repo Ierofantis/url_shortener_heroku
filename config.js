0c532471ab5a377e64b03a1ea132fae998c8fe99
Ierofantis <evil69669@gmail.com>
2020-08-01 19:58:55 +0300

    change web config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://localhost:8080/';
+config.webhost = 'https://urlshortenerierofantis.herokuapp.com/';
 
 config.db.host = 'localhost';
 config.db.name = 'url_shortener';
